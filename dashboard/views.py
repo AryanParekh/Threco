be15d00355afacc53a5b00306f503bbcb8915c26
Aryan <aryanparekh2412@gmail.com>
2021-05-01 16:12:04 +0530

    minor correction in status pie chart

--- dashboard/views.py
+++ dashboard/views.py
@@ -81,7 +81,7 @@ def updatelist(request,id):
         wastepie = [['Waste','Carbon Emission Saved']]
         statuspie = [["Status","No. of Updates"]]
         from django.db.models import Count
-        result = Update.objects.values('status').annotate(dcount=Count('status')).order_by()
+        result = Update.objects.filter(company=company).values('status').annotate(dcount=Count('status')).order_by()
         for i in result:
             statuspie.append([i['status'],i['dcount']])
         wasteset = dict()

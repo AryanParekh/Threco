fccda49f2fe80f229ec0ece81b1be8548459bb0c
AryanParekh <aryanparekh2412@gmail.com>
2021-11-29 18:16:48 +0000

    before changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.home,name="home"),
     path("adminlogin/",views.adminlogin,name='adminlogin'),
-    path("clientlogin/",views.clientlogin,name='clientlogin'),
+    path("",views.clientlogin,name='clientlogin'),
     path("companylist/",views.companylist,name='companylist'),
     path("updatelist/<int:id>/",views.updatelist,name='updatelist'),
     path("update_detail/<int:id>/",views.update_detail,name='update_detail'),
